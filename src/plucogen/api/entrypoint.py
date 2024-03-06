d9aee9da6c4783cedc98bf9032b01b0b92411a43
Jörn Krenzer <joern.krenzer@gmx.de>
2024-03-06 18:24:28 +0000

    Enhanced entrypoints

--- src/plucogen/api/entrypoint.py
+++ src/plucogen/api/entrypoint.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from importlib import metadata
-from typing import List, Tuple, Union
+from typing import List, Tuple, Union, Type
 
 from plucogen.logging import getLogger
 
@@ -9,22 +9,22 @@ log = getLogger(__name__)
 
 @dataclass
 class Entrypoints(object):
-    prefix: Tuple[str] = tuple()
+    prefix: Tuple[str, ...] = tuple()
 
     @classmethod
-    def get(cls, group_path: Tuple[str] = tuple()) -> List:
+    def get(cls, group_path: Tuple[str, ...] = tuple()) -> metadata.EntryPoints:
         group = ".".join(cls.prefix + group_path)
         log.debug("Looking up entrypoints for %s", group)
         return metadata.entry_points().select(group=group)
 
     @classmethod
-    def get_all(cls) -> List:
+    def get_all(cls) -> metadata.EntryPoints:
         return metadata.entry_points()
 
     @classmethod
     def create_entrypoints(
-        cls, group_prefix_path: Union[Tuple[str], str] = None
-    ) -> "Entrypoints":
+        cls, group_prefix_path: Union[Tuple[str, ...], str, None] = None
+    ) -> "Type[Entrypoints]":
         if isinstance(group_prefix_path, (str, None)):
             if group_prefix_path is None:
                 group_prefix_path = __name__
@@ -32,8 +32,9 @@ class Entrypoints(object):
             if len(group_prefix_path) == 1:
                 group_prefix_path = cls.prefix + group_prefix_path[1:]
         log.debug("Creating entrypoints for %s", ".".join(group_prefix_path))
+        suffix = "".join(s.title() for s in group_prefix_path)
         return type(
-            "Entrypoints",
+            "Entrypoints" + suffix,
             (cls,),
             {"prefix": group_prefix_path},
         )
